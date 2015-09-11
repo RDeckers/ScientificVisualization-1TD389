0ea666c395243cfe46fc1c0e2b844e0a9ae323b0
R. Deckers <admin@codingcat.nl>
2015-09-12 01:31:05 +0200

    We have a picture :D

--- Assignments/part1/isosurface.py
+++ Assignments/part1/isosurface.py
@@ -8,7 +8,7 @@ python isosurface.py
 
 """
 
-import vtk
+from vtk import *
 
 #needed to determine the path to the source files
 from os.path import dirname, realpath, join
@@ -23,19 +23,17 @@ class KeyboardInterface(object):
 
     """
 
-    def __init__(self):
+    def __init__(self): #TODO: Read up op Python classes, move initialization into here, absorb an relevant variables.
         self.screenshot_counter = 0
-        self.render_window = None
         self.window2image_filter = None
         self.png_writer = None
-        # Add the extra attributes you need here...
 
     def keypress(self, obj, event):
         """This function captures keypress events and defines actions for
         keyboard shortcuts."""
         key = obj.GetKeySym()
         if key == "9":
-            self.render_window.Render()
+            render_window.Render()
             self.window2image_filter.Modified()
             screenshot_filename = ("screenshot%02d.png" %
                                    (self.screenshot_counter))
@@ -43,12 +41,12 @@ class KeyboardInterface(object):
             self.png_writer.Write()
             print("Saved %s" % (screenshot_filename))
             self.screenshot_counter += 1
-        # Add your keyboard shortcuts here. You can use, e.g., the
-        # "Up" key to increase the isovalue and the "Down" key to
-        # decrease it. Don't forget to call the render window's
-        # Render() function to update the rendering after you have
-        # changed the isovalue.
-        # elif key == ...
+        elif key == "1":
+            contour_filter.SetValue(0, 0.7)
+            render_window.Render()
+        elif key == "2":
+            contour_filter.SetValue(0, 0.3)
+            render_window.Render()
 
 
 # Read the volume dataset
@@ -78,17 +76,22 @@ outline_actor.SetMapper(outline_mapper)
 outline_actor.GetProperty().SetColor(0.8, 0.8, 0.8)
 outline_actor.GetProperty().SetLineWidth(2.0)
 
-#
-#
-# Add your isosurface extraction and colormapping code here...
-#
-#
+contour_filter = vtkContourFilter()
+contour_filter.SetInputConnection(reader.GetOutputPort())
+contour_filter.SetValue(0, 0.5)
+
+surface_mapper = vtkPolyDataMapper()
+surface_mapper.SetInputConnection(contour_filter.GetOutputPort())
+
+surface_actor = vtkActor()
+surface_actor.SetMapper(surface_mapper)
+
 
 # Create a renderer and add the actors to it
 renderer = vtk.vtkRenderer()
 renderer.SetBackground(0.2, 0.2, 0.2)
 renderer.AddActor(outline_actor)
-# renderer.AddActor(...)
+renderer.AddActor(surface_actor)
 
 # Create a render window
 render_window = vtk.vtkRenderWindow()
@@ -109,8 +112,7 @@ png_writer.SetInput(window2image_filter.GetOutput())
 
 # Set up the keyboard interface
 keyboard_interface = KeyboardInterface()
-keyboard_interface.render_window = render_window
-keyboard_interface.window2image_filter = window2image_filter
+keyboard_interface.window2image_filter = window2image_filter #TODO: Move these out of the way
 keyboard_interface.png_writer = png_writer
 
 # Connect the keyboard interface to the interactor
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Join the chat at https://gitter.im/UppsalaStudents/ScientificVisualization-1TD389](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/UppsalaStudents/ScientificVisualization-1TD389?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+[![Join the chat at https://gitter.im/UppsalaStudents/ScientificVisualization-1TD389](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/UppsalaStudents/ScientificVisualization-1TD389?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) (<-- General Course Chatroom, from which this repo is forked)
 
-Scientific Visualization (1TD389) at Uppsala University. If you haven't already, please read the [introduction](https://github.com/UppsalaStudents/Introduction/blob/master/README.md) of this group.
+My work for Scientific Visualization (1TD389) at Uppsala University. Forked from [Uppsala Students](https://github.com/UppsalaStudents/ScientificVisualization-1TD389).
